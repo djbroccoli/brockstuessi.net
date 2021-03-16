c14a60733ea5c0949110bc702a3c741c13ad7a6c
Brock <brock@warpmail.net>
2021-03-16 16:00:01 -0700

    style changes, image uploads

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Author: Brock Stuessi
+//Date: 2/12/2020
+
+$(document).ready(function(){
+
+
+
+
+
+})
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-{
-let windowHeight = $(window).height();
 
-
-$('.sections').css('height', windowHeight);
-
-}
-{
 //Typewriter effect
 
 //For creating html retroactivly from the typewriter
@@ -18,44 +11,24 @@ var paper = document.getElementById("typewriter");
 //create new instance of Typewriter
 //source:
 var typewriter = new Typewriter(paper, {
- delay: 20,
+ delay: 100,
  onCreateTextNode: customNodeCreator,
  wrapperClassName: "typedText",
  cursorClassName: "typer",
+
 });
 
 
-typewriter.typeString("<h1>Hi there, you have arrived at Brock Stuessi's personal website.</h1>");
-typewriter.pauseFor(300);
-typewriter.typeString("<h1>. . .</h1>");
-typewriter.pauseFor(300);
-typewriter.typeString("<h1>Thanks for stopping by, are you here to learn more about his:</h1>");
-typewriter.pauseFor(300);
-typewriter.typeString("<a href=\"#music\" class=\"main-links\">Music</a>");
-typewriter.pauseFor(300);
-typewriter.typeString("<a href=\"#archives\" class=\"main-links\">Archival Work</a>");
-typewriter.pauseFor(300);
-typewriter.typeString("<a href=\"#writing\" class=\"main-links\">Writing</a>");
-typewriter.pauseFor(300);
-typewriter.typeString("<a href=\"#web\" class=\"main-links\">Web Projects</a>")
-typewriter.pauseFor(300);
-typewriter.typeString("<h1>Or, would you just like to: <a href=\"\">get in touch?</a></h1>");
-typewriter.start();
+typewriter.typeString("<h1>Brock Stuessi</h1>")
+.pauseFor(300)
+.typeString("<img src='imgs/email.png'>")
+.pauseFor(300)
+.typeString("<img src='imgs/cv.png'>")
+.pauseFor(300)
+.typeString("<img src='imgs/phone.png'>")
+.start();
 
 
-}
-
-
-
-
-$('#enter-site').click(function() {
-  $('#typewriter').toggleClass("hidden");
-
-
-
-
-
-})
 
 
 })
